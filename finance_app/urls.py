409aa3613c8f098cb33e36d3e14a47669b1ecf11
Oakley Cardwell <oakleydc123@gmail.com>
2024-11-08 00:26:03 -0600

    INIT

---
+++ finance_app/urls.py
@@ -0,0 +1,29 @@
+"""
+URL configuration for finance_app project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    # path('admin/', admin.site.urls),
+    # path('users/', include('users.urls')),
+    # path('transactions/', include('transactions.urls')),
+    # path('budgets/', include('budgets.urls')),
+    # path('goals/', include('goals.urls')),
+    # path('reports/', include('reports.urls')),
+    # Default route can point to a home app or view
+    # path('', include('home.urls')),  # Create a 'home' app for landing pages
+]
